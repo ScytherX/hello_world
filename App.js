b6bbc51eb15a6101ea908a79158290d3560dd07b
ScytherX <inagatari@gmail.com>
2021-07-04 10:39:49 -0500

    Actualizacion App hello world
    Se agrego el codigo de la app hello world

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 export default function App() {
-return (
-<View style={styles.container}>
-<Text >Hello World</Text>
-</View>
-);
+    return (
+        <View style={styles.container}>
+            <Text >Hello World</Text>
+        </View>
+    );
 }
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-justifyContent: 'center',
-alignItems: 'center',
-backgroundColor: '#F5FCFF',
-},
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
 })
