d7ecac8099b60e3fe2111b80240c2c95d00b943e
ScytherX <inagatari@gmail.com>
2021-10-05 23:00:28 -0600

    Ya nada mas faltan los eventos

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { Fragment } from 'react';
+import { StyleSheet, Text, Button, Image } from 'react-native';
 export default function App() {
+
+    const handleDeploy = (e) => {
+        return (
+            <Fragment style={styles.separador}>
+                <Image source={require('./UPENLOGO.png')} style={styles.imge} />
+                <Text style={styles.separador}>Universidad Politécnica del Estado de Nayarit</Text>
+            </Fragment>
+        );
+    }
+
+    const handleLeft = (e) => {
+
+    }
+
+    const handleRight = (e) => {
+
+    }
+
     return (
-        <View style={styles.container}>
-            <Text >Universidad Politécnica del Estado de Nayarit</Text>
-        </View>
+        <Fragment style={styles.container}>
+
+
+            <Button title="Boom baby" onPress={handleDeploy} />
+
+            <Button title="Falcon speaks" onPress={handleLeft} />
+
+            <Button title="Pie bueno" onPress={handleRight} />
+
+        </Fragment>
     );
 }
 const styles = StyleSheet.create({
@@ -14,4 +39,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
+    separador: {
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        margin: 30,
+        fontSize: 17
+    },
+    imge: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        width: 200,
+        height: 200,
+        margin: 20
+    }
 })
